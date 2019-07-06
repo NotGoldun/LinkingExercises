fdcf878208deff7128ae0aa4342212d4e5534e9f
NotGoldun <domeniko.sutlovic@gmail.com>
2019-07-06 13:47:16 +0200

    Memory allocation exercise

---
+++ Application.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <memory>
+
+class Entity
+{
+public:
+	Entity();
+	~Entity();
+
+private:
+
+};
+
+int main()
+{
+	int* someNumber = (int*)malloc(sizeof(int));
+
+	free(someNumber);
+
+	int* newNumber = new int;
+
+	delete newNumber;
+
+	Entity* mallocEntity = (Entity*)malloc(sizeof(Entity));
+
+	free(mallocEntity);
+
+	Entity* newEntity = new Entity();
+	
+	delete newEntity;
+
+	//Summary: Malloc ne poziva constructor klase, free ne poziva destructor klase
+	//Summary: New poziva constructor, delete poziva destructor
+}
